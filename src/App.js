19680315d88f32c0cbad3d064989679fe24514ba
FinnBuick <finnbuick@gmail.com>
2018-07-13 16:20:36 +1000

    Implemented oauth, fixed css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import queryString from 'query-string'
 import './App.css'
 import LandingPage from './LandingPage';
 import MainPage from './MainPage';
+import testUserData from './user.json';
 
 class Footer extends Component{
   render() {
@@ -14,10 +16,27 @@ class Footer extends Component{
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+        serverData: {}
+    }
+  }
+
+  componentDidMount() {
+    let parsed = queryString.parse(window.location.search)
+    let accessToken = parsed.access_token
+
+    fetch('https://api.spotify.com/v1/me', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then((response) => response.json())
+    .then(data => console.log(data);)
+  }
+
   render() {
     return (
       <div className='App uk-container'>
-        <MainPage />
+        { testUserData.display_name ? <MainPage /> : <LandingPage />}
         <Footer />
       </div>
     );
