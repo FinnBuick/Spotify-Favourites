25768767dabf89fe1c7c1d1c54de79f3db9bb4a3
Finn Buick <finnbuick@gmail.com>
2018-08-02 19:48:18 +1000

    minor changes

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -7,7 +7,7 @@ class LandingPage extends Component {
       <div className="LandingPage">
         <div className="uk-position-center">
           <h1 className="uk-heading-primary">Spotify Favourites</h1>
-          <p>Find out what your most played tracks are</p>
+          <p>Find out your most played tracks</p>
           <button onClick={() => window.location = 'https://spotify-favourites-backend.herokuapp.com/login'}
             className="uk-button uk-button-secondary uk-button-large">Get Started</button>
         </div>
